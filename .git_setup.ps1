24a7145f672ea57e99accee0d358170d32e4197d
JOE <joelin818818@163.com>
2026-08-24 15:03:35 +0800

    chore: remove temp git helper scripts

--- .git_setup.ps1
+++
@@ -1,7 +0,0 @@
-cd d:/Personal/szjm/Documents/cursor/CF_SITE_IP
-git init
-git remote add origin https://github.com/joelin818818/cf-probe-select.git
-git add -A
-git commit -q -m "init: cf-probe-select base crawler and project scaffold"
-git branch -M main
-git push -u origin main 2>&1 | Out-String | Write-Output
